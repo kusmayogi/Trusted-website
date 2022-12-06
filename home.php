fa7f15a936f9eab669e438afae67bee04fafa09b
syailendra <syailendrakusmayogi@gmail.com>
2022-12-06 23:01:41 +0700

    new update home fix and notif page

--- home.php
+++ home.php
@@ -3,13 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="	css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Trusted: Home</title>
-  </head>
-  <body>
-  <nav>
+    <title>Trusted: Login</title>
+    <nav>
         <div class="logo-name">
             <div class="logo-image">
                 <img src="TRST.png" width="200">
@@ -17,7 +15,7 @@
         </div>
         <div class="menu-items">
             <ul class="nav-links">
-                <li><a href="">
+                <li><a href="index.html">
                     <i class="uil uil-estate"></i>
                         <span class="link-name">Home</span>
                     </a></li>
@@ -33,18 +31,145 @@
                     <i class="uil uil-edit"></i>
                         <span class="link-name">Data Barang</span>
                     </a></li>
-                <li><a href="laporan.php">
+                <li><a href="#">
                     <i class="uil uil-file-info-alt"></i>
                         <span class="link-name">Laporan</span>
                     </a></li>
-                <li><a href="akun.php">
+                <li><a href="#">
                     <i class="uil uil-user"></i>
                         <span class="link-name">Akun</span>
                     </a></li>
             </ul>
         </div>
     </nav>
+    <section class="dashboard">
+        <div class="top">
+            <i></i>
+                <i></i>
+                <i></i>
+                <i></i>
+                <i></i>
+                <i></i>
+                <i></i>
+                <i></i>
+                <button type="button"  href="notif.php" class="btn btn-light"> <a href="notif.php"><i class="uil uil-bell"></i></a></button>
+            <button type="button" class="btn btn-light"><img src="lutpi1.png" alt="">  Toko Bang Lutfi</button>
+        </div>
+        <div class="row">
+        <div class="col-4">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Pesanan Hari Ini</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Pemasukan Hari Ini</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row mt-3">
+        <div class="col-4">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Produk Terjual Bulan Ini</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Jumlah Produk</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+        </div>
+        </div>
+        <div class="col">
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nama Produk</th>
+      <th scope="col">Tanggal</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+    </tbody>
+</table>
+    </div>
     
-    <script type="text/javascript" src="js/bootstrap.bundle.min.js>"></script>
-  </body>
-  </html>
+</div>
+<div class="row mt-4">
+    <div class="col-6 text-center">
+<p class="h5">Produk Unggulan Tokomu</p>
+</div>
+</div>
+<div class="row mt-2">
+    <div class="col-2">
+    <div class="card mt-3" style="width: 10rem;">
+  <img src="beigge.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-2">
+    <div class="card mt-3" style="width: 10rem;">
+  <img src="hitam1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+    </div>
+    <div class="col-3">
+    <div class="card mt-3" style="width: 10rem;">
+  <img src="hitam2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+    </div>
+</div>
+</section>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js>"></script>
+    </body>
+    </html>
---
+++ notif.php
@@ -0,0 +1,66 @@
+<?php
+require "koneksi.php"
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Notification</title>
+  </head>
+  <body>
+    <div class="container-fluid mt-3">
+    <button type="button" class="btn btn-light"><i class="uil uil-step-backward"></i>  Notification</button>
+    </div>
+    <div class="container-fluid">
+    <div class="row align-items-center mt-4">
+    <div class="col">
+    <table class="table">
+  <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td></td>
+      <td><button type="button" class="btn btn-danger">Danger</button></td>
+    </tr>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+    </div>
+    </div>
+  <script type="text/javascript" src="js/bootstrap.bundle.min.js>"></script>
+    </body>
+    </html>
